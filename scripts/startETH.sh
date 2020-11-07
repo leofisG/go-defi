3c2ae1a8decd02215ce879c85337c31db0d12a31
Leonard Ge <gw980531@gmail.com>
2020-11-07 14:27:32 +0800

    Added ganache startup script and action.

---
+++ scripts/startETH.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+npm install -g ganache-cli
+ganache-cli -f http://watson.lowland.fun:19545/ \
+  -m "clutch captain shoe salt awake harvest setup primary inmate ugly among become" \
+  -i 1 \
+  -u 0x9759A6Ac90977b93B58547b4A71c78317f391A28 \
