9b2e7fc7854cc87a4b6845a0d60a42f1ae40db89
Leonard Ge <gw980531@gmail.com>
2020-11-08 09:50:04 +0800

    Committed tempt.

--- client/client_test.go
+++ client/client_test.go
@@ -13,7 +13,6 @@ import (
 	"github.com/ethereum/go-ethereum/ethclient"
 )
 
-// Test inbox contract gets deployed correctly
 func TestInteractWithCompound(t *testing.T) {
 	key, err := crypto.HexToECDSA("b8c1b5c1d81f9475fdf2e334517d29f733bdfa40682207571b12fc1142cbf329")
 	if err != nil {
@@ -49,6 +48,41 @@ func TestInteractWithCompound(t *testing.T) {
 
 	if cETH == big.NewInt(0) {
 		log.Fatal("CTH minting is not successful")
+	}
+
+	afterETH, err := ethClient.BalanceAt(context.Background(), fromAddr, nil)
+
+	if beforeETH.Cmp(afterETH) != 1 {
+		log.Fatalf("ETH balance not decreasing.")
+	}
+}
+
+func TestInteractWithUniswap(t *testing.T) {
+	key, err := crypto.HexToECDSA("b8c1b5c1d81f9475fdf2e334517d29f733bdfa40682207571b12fc1142cbf329")
+	if err != nil {
+		log.Fatalf("Failed to create private key: %v", err)
+	}
+	ethClient, err := ethclient.Dial("http://127.0.0.1:8545")
+	if err != nil {
+		log.Fatalf("Failed to connect to ETH: %v", err)
+	}
+
+	publicKeyECDSA, ok := (key.Public()).(*ecdsa.PublicKey)
+	if !ok {
+		log.Fatal("cannot assert type")
+	}
+	fromAddr := crypto.PubkeyToAddress(*publicKeyECDSA)
+	beforeETH, err := ethClient.BalanceAt(context.Background(), fromAddr, nil)
+
+	auth := bind.NewKeyedTransactor(key)
+	defiClient, err := NewClient(auth, ethClient, MainNet)
+	if err != nil {
+		t.Errorf("Error creating client: %v.", err)
+	}
+
+	err = defiClient.Uniswap().Swap(1e18, DAI, ETH, fromAddr)
+	if err != nil {
+		log.Fatalf("Failed to swap in uniswap: %v", err)
 	}
 
 	afterETH, err := ethClient.BalanceAt(context.Background(), fromAddr, nil)
