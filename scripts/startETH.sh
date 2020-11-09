ae2f83b05ffa1b83ac06016b384fbdefac18e01b
Leonard Ge <gw980531@gmail.com>
2020-11-09 13:28:30 +0800

    Fixed bug in startETH script.

--- scripts/startETH.sh
+++ scripts/startETH.sh
@@ -7,6 +7,6 @@ ganache-cli -q \
   -f http://watson.lowland.fun:19545/ \
   -m "clutch captain shoe salt awake harvest setup primary inmate ugly among become" \
   -i 1 \
-  --unlock 0x39aa39c021dfbae8fac545936693ac917d5e7563
-  --unlock 0xa0df350d2637096571F7A701CBc1C5fdE30dF76A
+  --unlock 0x39aa39c021dfbae8fac545936693ac917d5e7563 \
+  --unlock 0xa0df350d2637096571F7A701CBc1C5fdE30dF76A \
   -u 0x9759A6Ac90977b93B58547b4A71c78317f391A28 &
